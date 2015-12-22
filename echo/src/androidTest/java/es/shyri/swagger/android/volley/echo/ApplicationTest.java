d0b5f7f18299dae8712c971d390ad3c21b170c8f
Shyri Villar <igs.villar@gmail.com>
2015-12-22 03:28:51 +0100

    Complete echo example

--- echo/src/androidTest/java/es/shyri/swagger/android/volley/echo/ApplicationTest.java
+++ echo/src/androidTest/java/es/shyri/swagger/android/volley/echo/ApplicationTest.java
@@ -3,11 +3,86 @@ package es.shyri.swagger.android.volley.echo;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import net.jodah.concurrentunit.Waiter;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeoutException;
+
+import io.swagger.client.ApiInvoker;
+import io.swagger.client.api.DefaultApi;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    ApiInvoker apiInvoker;
+    DefaultApi defaultApi;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void getTest() throws TimeoutException {
+        final Waiter waiter = new Waiter();
+        defaultApi.rootGet(new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                waiter.assertTrue(true);
+                waiter.resume();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                waiter.assertTrue(false);
+                waiter.resume();
+            }
+        });
+        waiter.await();
+    }
+
+    @Test
+    public void postText() throws TimeoutException {
+        final Waiter waiter = new Waiter();
+        defaultApi.rootPost("name", "2005", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                waiter.assertTrue(true);
+                waiter.resume();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                waiter.assertTrue(false);
+                waiter.resume();
+            }
+        });
+        waiter.await();
+    }
+
+    @Test
+    public void testPathIdGet() throws TimeoutException {
+        final Waiter waiter = new Waiter();
+        defaultApi.testPathIdGet("1", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                waiter.assertTrue(true);
+                waiter.resume();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                waiter.assertTrue(false);
+                waiter.resume();
+            }
+        });
+        waiter.await();
     }
 }
--- echo/src/main/java/io/swagger/client/Responses.java
+++ echo/src/main/java/io/swagger/client/Responses.java
@@ -1,7 +1,5 @@
 package io.swagger.client;
 
-import java.util.List;
-import io.swagger.client.model.*;
 
 public class Responses {
   
