0e2deb3f528265d1ea04472600cd4baddf0dc1d2
Shyri Villar <igs.villar@gmail.com>
2015-12-23 18:45:35 +0100

    Fix echo sample tests

--- echo/src/androidTest/java/es/shyri/swagger/android/volley/echo/ApplicationTest.java
+++ echo/src/androidTest/java/es/shyri/swagger/android/volley/echo/ApplicationTest.java
@@ -1,6 +1,7 @@
 package es.shyri.swagger.android.volley.echo;
 
 import android.app.Application;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
 
 import com.android.volley.Response;
@@ -8,16 +9,21 @@ import com.android.volley.VolleyError;
 
 import net.jodah.concurrentunit.Waiter;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeoutException;
 
 import io.swagger.client.ApiInvoker;
 import io.swagger.client.api.DefaultApi;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
     ApiInvoker apiInvoker;
     DefaultApi defaultApi;
@@ -25,6 +31,14 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Before
+    public void setUp() {
+        ApiInvoker.initializeInstance(getInstrumentation().getContext());
+        apiInvoker = ApiInvoker.getInstance();
+        defaultApi = new DefaultApi();
+    }
+
 
     @Test
     public void getTest() throws TimeoutException {
